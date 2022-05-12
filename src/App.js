68ea5a840ee14a08135fa0f8b82834ddd4064dc4
nguyenhuytntc <nguyenhuytntc@gmail.com>
2022-05-13 05:33:16 +0700

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRouters, privateRouters } from '~/routes';
+import { DefaultLayout, HeaderOnly } from '~/components/Layout';
 function App() {
     return (
-        <div className="App">
-            <h1>Hello Anh em</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRouters.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
