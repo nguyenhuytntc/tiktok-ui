ad374a5e2748907eb1e13dce27c487e8b9ec7eaf
huynguyentntc <nguyenhuytntc@gmail.com>
2022-05-12 19:55:25 +0700

    css reset done

--- src/component/GlobalStyles/index.js
+++ src/component/GlobalStyles/index.js
@@ -1,4 +1,4 @@
-import './GlobalStyles';
+import './GlobalStyles.scss';
 
 function GlobalStyles({ children }) {
     return children;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '~/App';
+import GlobalStyles from './component/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
     </React.StrictMode>,
 );
