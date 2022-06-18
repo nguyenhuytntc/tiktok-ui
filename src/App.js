9de7ee603d60d855df29af2e851cded13f85de42
huynguyen <nguyenhuytntc@gmail.com>
2022-06-19 04:53:03 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRouters } from '~/routes';
 import DefaultLayout from '~/layouts';
 function App() {
